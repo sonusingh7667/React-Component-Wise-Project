355e94b2737152c263df01a6c4dd2e500db16268
Sonu Singh <153010657+sonusingh7667@users.noreply.github.com>
2026-01-24 22:44:55 +0530

    Add files via upload

---
+++ ReactQ40Interview/TabNavigation.jsx
@@ -0,0 +1,48 @@
+import React , {useState} from 'react'
+
+const TabNavigation = () => {
+
+    const [activeTab, setActiveTab] = useState("Home");
+
+  return (
+    <div>
+      <h1>Tab Navigation</h1>
+
+      <button onClick={() => setActiveTab("Home")}>Home</button>
+      <button onClick={() => setActiveTab("profile")}>Profile</button>
+      <button onClick={() => setActiveTab("setting")}>Setting</button>
+      <button onClick={() => setActiveTab("carts")}>Carts</button>
+
+
+      {activeTab === "Home" && (
+        <div>
+            <h2>Home</h2>
+            <p>Welcome to the home section</p>
+        </div>
+      )}
+
+      {activeTab === "profile" && (
+        <div>
+            <h2>Profile</h2>
+            <p>welcome to profile section</p>
+        </div>
+      )}
+
+      {activeTab === "setting" && (
+        <div>
+            <h2>setting</h2>
+            <p>Lorem ipsum dolor sit amet consectetur </p>
+        </div>
+      )}
+
+      {activeTab === "carts" && (
+        <div>
+          <h2>Carts</h2>
+          <p>this is carts section please check it outtyuty</p>
+          </div>
+      )}
+    </div>
+  )
+}
+
+export default TabNavigation
